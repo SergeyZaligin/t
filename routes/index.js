b7874df0afebb86e70c57d79942cf1640de11499
Sergey <codename.cobweb@gmail.com>
2018-12-09 16:58:06 +0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/index');
+var guardRoutes = require('../middleware/guardRoutes');
 
 /* GET home page. */
 router.get('/', controller.index);
-router.get('/about', controller.about);
+router.get('/about', guardRoutes.guardAdminRoutes, controller.about);
 
 module.exports = router;
